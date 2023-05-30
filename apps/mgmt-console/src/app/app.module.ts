19cba7c6450ca073fc98249c2f71b4d8c55626a3
Jade Hollowell <jade.hollowell@66degrees.com>
2023-05-30 10:25:59 -0700

    remove nx welcome component

--- apps/mgmt-console/src/app/app.module.ts
+++ apps/mgmt-console/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { CheckboxComponentModule } from "@mgmt-ui-components/form";
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     CheckboxComponentModule,
