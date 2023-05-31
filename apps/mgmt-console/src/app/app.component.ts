673f04333494f526f55de6ea14c8f5ab71247575
Jade Hollowell <jade.hollowell@66degrees.com>
2023-05-31 10:48:53 -0700

    checkboxes, update favicon, update readme

--- apps/mgmt-console/src/app/app.component.ts
+++ apps/mgmt-console/src/app/app.component.ts
@@ -9,17 +9,27 @@ import notify from 'devextreme/ui/notify';
 })
 export class AppComponent {
   title = 'mgmt-console';
+
   // Checkbox
   chkboxName = 'testChkbox';
-  chkboxText = 'Check Me!';
-  
-  constructor(@Inject(DOCUMENT) private document: Document) { 
+  darkModeLabel = 'Dark mode';
+  lightModeLabel = 'Light mode';
+  lightModeId = 'lightMode';
+  darkModeId = 'darkMode';
+  isFalse = false;
+  isNull = null;
+  isTrue = true;
+  smIconSize = 16;
+  mdIconSize = 20;
+  tabindex = 1;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
     this.document.body.classList.add('light-mode');
   }
 
   capitalize = (text: string) => text.charAt(0).toUpperCase() + text.slice(1);
-  
-  chkboxClick = (e: any) => {
+
+  chkboxClick1 = (e: any) => {
     if(e.value) {
       this.document.body.classList.add('light-mode');
       this.document.body.classList.remove('dark-mode');
@@ -27,6 +37,19 @@ export class AppComponent {
     else {
       this.document.body.classList.add('dark-mode');
       this.document.body.classList.remove('light-mode');
+    }
+    const chkboxName = e.component.option('name');
+    notify(`The ${this.capitalize(chkboxName)} checkbox was clicked`);
+  };
+
+  chkboxClick2 = (e: any) => {
+    if(e.value) {
+      this.document.body.classList.add('dark-mode');
+      this.document.body.classList.remove('light-mode');
+    }
+    else {
+      this.document.body.classList.add('light-mode');
+      this.document.body.classList.remove('dark-mode');
     }
     const chkboxName = e.component.option('name');
     notify(`The ${this.capitalize(chkboxName)} checkbox was clicked`);
--- libs/mgmt-ui-components/src/lib/form/checkbox/checkbox.component.ts
+++ libs/mgmt-ui-components/src/lib/form/checkbox/checkbox.component.ts
@@ -9,12 +9,20 @@ import { DxCheckBoxModule } from 'devextreme-angular';
 })
 export class CheckboxComponent {
   // Checkbox API reference: https://js.devexpress.com/Documentation/ApiReference/UI_Components/dxCheckBox/Configuration/
+  @Input() ariaLabel?: string;
+  @Input() ariaLabelledBy?: string;
+  @Input() id?: string;
+  // For a small checkbox, pass 16 to the iconSize property.
+  // For a medium checkbox, pass 20 to the iconSize property.
   @Input() iconSize?: number | string;
-  @Input() name?: string;
-  @Input() tabIndex?: number;
-  @Input() text?: string;
+  @Input() isDisabled!: boolean;
+  @Input() name!: string;
+  @Input() tabindex?: number;
   @Input() validationStatus?: string;
-  @Input() value?: boolean;
+  // For a checked checkbox, pass true as the value.
+  // For an unchecked checkbox, pass false as the value.
+  // For a checkbox in an indeterminate state, pass null as the value.
+  @Input() value?: boolean | null;
   // eslint-disable-next-line @angular-eslint/no-output-on-prefix
   @Output() onValueChanged: EventEmitter<string> = new EventEmitter();
 
