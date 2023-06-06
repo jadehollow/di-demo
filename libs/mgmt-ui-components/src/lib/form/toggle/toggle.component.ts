e4fb38b08253bcc33d158ebba07d8d1e6ee6ac1c
willgarvey66 <133800127+willgarvey66@users.noreply.github.com>
2023-06-06 08:08:27 -0500

    Cleaning up comments

--- libs/mgmt-ui-components/src/lib/form/toggle/toggle.component.ts
+++ libs/mgmt-ui-components/src/lib/form/toggle/toggle.component.ts
@@ -32,80 +32,12 @@ export class ToggleComponent {
   @Output() onValueChanged = new EventEmitter<any>();
 
   handle = (e: any) => {
-    // console.log(e.value); // e.value is the output (true/false)
     this.onValueChanged.emit(e);
   };
 
-  // @Output() onchange: EventEmitter<ToggleOnChangeEvent> = new EventEmitter();
-
-  // modelValue: any = false;
-
-  // focused = false;
-
-  // onModelChange: Function = () => {};
-
-  // onModelTouched: Function = () => {};
-
-  // constructor(private cd: ChangeDetectorRef) {}
-
-  // onClick(event: Event, cb: HTMLInputElement) {
-  //   if (!this.disabled && !this.readonly) {
-  //     event.preventDefault();
-  //     this.toggle(event);
-  //     cb.focus();
-  //   }
-  // }
-
-  // onInputChange(event: Event) {
-  //   if (!this.readonly) {
-  //     const inputChecked = (<HTMLInputElement>event.target).checked;
-  //     this.updateModel(event, inputChecked);
-  //   }
-  // }
-
-  // toggle(event: Event) {
-  //   this.updateModel(event, !this.checked());
-  // }
-
-  // updateModel(event: Event, value: boolean) {
-  //   this.modelValue = value ? this.trueValue : this.falseValue;
-  //   this.onModelChange(this.modelValue);
-  //   this.onchange.emit({
-  //     originalEvent: event,
-  //     checked: this.modelValue,
-  //   });
-  // }
-
-  // onFocus(event: Event) {
-  //   this.focused = true;
-  // }
-
-  // onBlur(event: Event) {
-  //   this.focused = false;
-  //   this.onModelTouched();
-  // }
-
-  // writeValue(value: any): void {
-  //   this.modelValue = value;
-  //   this.cd.markForCheck();
-  // }
-
-  // registerOnChange(fn: Function): void {
-  //   this.onModelChange = fn;
-  // }
-
-  // registerOnTouched(fn: Function): void {
-  //   this.onModelTouched = fn;
-  // }
-
-  // setDisabledState(val: boolean): void {
-  //   this.disabled = val;
-  //   this.cd.markForCheck();
-  // }
-
-  // checked() {
-  //   return this.modelValue === this.trueValue;
-  // }
+  setDisabledState(val: boolean): void {
+    this.disabled = val;
+  }
 }
 
 @NgModule({
