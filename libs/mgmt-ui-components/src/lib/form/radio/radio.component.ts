8d06c7d03b8a963b0449ccf4728acf0203602c7d
oscar-jimenez-rccl <ojimenez@rccl.com>
2023-06-06 11:26:06 -0600

    radio fixes

--- libs/mgmt-ui-components/src/lib/form/radio/radio.component.ts
+++ libs/mgmt-ui-components/src/lib/form/radio/radio.component.ts
@@ -1,29 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { NgModule, Component, EventEmitter, Output, Input, forwardRef } from '@angular/core';
+import { NgModule, Component, EventEmitter, Input, Output } from '@angular/core';
 import { DxRadioGroupModule } from 'devextreme-angular';
-import { NG_VALUE_ACCESSOR, ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
-  selector: 'di-radio',
+  selector: 'di-radiobutton',
   templateUrl: './radio.component.html',
   styleUrls: ['./radio.component.scss'],
 })
-export class RadioComponent {
+export class DiRadioComponent{
   @Input() items: any[];
-  @Input() displayExpr: string;
-  value = '';
-  //@Output() onValueChanged: EventEmitter<RadioButtonClickEvent> = new EventEmitter<RadioButtonClickEvent>();
+  @Input() displayExpr?: string;
+  @Input() value: any;
+  @Input() size: 'sm' | 'md';
+  @Input() layout: string;
+  @Input() disabled: boolean;
+  @Output() onValueChanged: EventEmitter<any> = new EventEmitter<any>();
   @Input() ariaLabel: string | undefined;
+  @Output() valueChange = new EventEmitter();
 
   constructor() {
     this.items = [];
-    this.displayExpr = '';
+    this.value = undefined;
+    this.size = 'md';
+    this.layout = 'vertical';
+    this.disabled = false;
+  }
+
+  handleValueChanged(event: any) {
+    if(this.onValueChanged) {
+      this.value = event.value;
+      this.valueChange.emit(this.value);
+      this.onValueChanged.emit(event);
+    }
   }
 }
 
 @NgModule({
   imports: [CommonModule, DxRadioGroupModule],
-  exports: [RadioComponent],
-  declarations: [RadioComponent]
+  exports: [DiRadioComponent],
+  declarations: [DiRadioComponent]
 })
 export class RadioComponentModule {}
