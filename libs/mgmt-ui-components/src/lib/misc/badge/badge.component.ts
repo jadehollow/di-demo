0b6bba90a85fe50253ef5f0228e826e6245e090f
willgarvey66 <133800127+willgarvey66@users.noreply.github.com>
2023-06-09 15:57:26 -0500

    badges

--- libs/mgmt-ui-components/src/lib/misc/badge/badge.component.ts
+++ libs/mgmt-ui-components/src/lib/misc/badge/badge.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, NgModule } from '@angular/core';
+import { DiIconComponent } from 'libs/mgmt-icon-lib/src/lib/di-icon/di-icon.component';
 
 @Component({
   selector: 'di-saas-fe-badge',
   templateUrl: './badge.component.html',
   styleUrls: ['./badge.component.scss'],
 })
-export class BadgeComponent {}
+export class BadgeComponent {
+  @Input() tabindex?: number;
+  @Input() inputId?: string;
+  @Input() ariaLabel?: string;
+  @Input() ariaLabelledBy?: string;
+  @Input() size: 'sm'|'md'|'lg' = 'sm';
+  @Input() icon: string = 'False';
+  @Input() iconTrailing: boolean = false;
+  @Input() risk: 'None'|'Neutral'|'Good'|'Low'|'Moderate'|'High'|'Critical' = 'None';
+  @Input() outline: boolean = false;
+  @Input() default: 'Default'|'Selected' = 'Default';
+}
