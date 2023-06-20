75c3f2e7438a8c17e40dfae6ac96938fd17ffc28
Kamil Gombos <kamil.gombos@pncl.tech>
2023-06-20 12:13:12 -0500

    fix compilation errors

--- libs/mgmt-ui-components/src/lib/misc/badge/badge.component.ts
+++ libs/mgmt-ui-components/src/lib/misc/badge/badge.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, NgModule } from '@angular/core';
-import { DiIconComponent } from 'libs/mgmt-icon-lib/src/lib/di-icon/di-icon.component';
+import { MgmtIconLibModule } from '@mgmt-icon-lib';
 
 @Component({
   selector: 'di-badge',
@@ -18,4 +18,12 @@ export class BadgeComponent {
   @Input() risk: 'None'|'Neutral'|'Good'|'Low'|'Moderate'|'High'|'Critical' = 'None';
   @Input() outline: boolean = false;
   @Input() default: 'Default'|'Selected' = 'Default';
+  @Input() label: string = '';
 }
+
+@NgModule({
+  imports: [CommonModule, MgmtIconLibModule],
+  exports: [BadgeComponent],
+  declarations: [BadgeComponent],
+})
+export class BadgeComponentModule {}
