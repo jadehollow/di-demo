2809eb787f6d00079361e82cb5861306e0cc382b
Jade Hollowell <jade.hollowell@66degrees.com>
2023-07-05 11:40:18 -0700

    css loader

--- libs/mgmt-storybook-host/.storybook/preview.js
+++ libs/mgmt-storybook-host/.storybook/preview.js
@@ -1,7 +1,7 @@
 import '!style-loader!css-loader!sass-loader!../src/assets/styles.scss';
 import '!style-loader!css-loader!sass-loader!node_modules/devextreme/dist/css/dx.light.css';
 
-import { themes } from '@storybook/theming';
+import { themes } from '@storybook/theming'
 
 export const parameters = {
     darkMode: {
